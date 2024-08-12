8e31b25cad53385e29227d27024dbd7b83a49e3e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 14:14:33 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Create an empty array called 'todoList' to store your to-do list items.
+let todoList = []
+
+// You will be implementing a loop that repeatedly displays possible options, takes user input, and performs actions accordingly. 
+// Print "Welcome to the To-Do List App"
+let greet = ("Welcome to the To-Do List App")
+console.log(greet)
+// Print the options 
+// 1 - display the list
+// 2 - add new task to end
+// 3 - add new task to beginning
+// 4 - delete first task
+// 5 - exit
+
+let display = ("Press 1 to display the list")
+console.log(display)
+let end = ("Press 2 to add a new task to the end of the list")
+console.log(end)
+let beginning = ("Press 3 to add a new task to the beginning of the list")
+console.log(beginning)
+let remove = ("Press 4 to delete the first task")
+console.log(remove)
+let exit = ("Press 5 to exit list")
+console.log(exit)
+
+let user = prompt("What do you choose to do?")
+parseInt(user)
+// Define a variable called userChoice and set it to 0
+let userChoice = 0
+
+// Create a while loop that runs while the user doesn't type in 5
+while (user != 5){
+  
+}
+
+// In the loop, use conditionals to match the action to the option 
+// 1 - display the list
+// 2 - add new task to end
+// 3 - add new task to beginning
+// 4 - delete first task
+// 5 - exit
+
+
+
+
+
+
+
+
+
